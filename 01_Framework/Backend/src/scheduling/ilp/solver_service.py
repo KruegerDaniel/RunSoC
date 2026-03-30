ae6905924e9fe0da0f9160e8f7c16ee5472ec0f4
Daniel Krueger <krugerdaniel2000@gmail.com>
2026-03-30 18:26:47 +0200

    Extended SoC JSON with cluster

--- 01_Framework/Backend/src/scheduling/ilp/solver_service.py
+++ 01_Framework/Backend/src/scheduling/ilp/solver_service.py
@@ -1,5 +1,6 @@
 import pulp
 
+from config import Config
 from scheduling.ilp.model_builder import build_model
 from scheduling.ilp.postprocess import extract_solution
 from schemas.schemas import ProblemInstance, Task
@@ -10,8 +11,8 @@ def solve_instance(problem: ProblemInstance):
     model, variables = build_model(problem)
 
     print("Solving problem")
-
-    solver = pulp.PULP_CBC_CMD(msg=True, keepFiles=True, logPath="cbc.log")
+    timelimit = Config.PULP_TIMELIMIT_SECONDS
+    solver = pulp.PULP_CBC_CMD(msg=True, keepFiles=True, logPath="cbc.log", timeLimit=timelimit)
     status = model.solve(solver)
 
     return extract_solution(problem, model, variables, status)
